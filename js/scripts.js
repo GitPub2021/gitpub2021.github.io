d6ab13db155d4b4ab6390e08e1981d6857d38a91
GitPub2021 <92309706+GitPub2021@users.noreply.github.com>
2022-01-21 21:47:44 -0500

    Updated scripts.js to use IIFE construct together with forEach() Construct for looping through heights

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-
-
-/* Define empty array for Pokemons */
-pokemonList = [] ;
-
-
 /* Create 3 Pokemon Objects as JavaScript Objects, here I am using Mario Kart Values as I dont know Pokemons;
 Each Object must be created a new and cannot be copied because of the difference
 between JavaScript Objects and StandardJavaScript Variables */
@@ -26,19 +20,38 @@ pokemon3 = {
   pokemonType: ['small','yellow']
 } ;
 
+/* Define IIFE for adding to and accessing of the pokemonList */
 
-/* Push one Object after the other into the array of Pokemons */
+let pokemonRepository = (function () {
+  let pokemonList = [];
 
-pokemonList.push(pokemon1);
-pokemonList.push(pokemon2);
-pokemonList.push(pokemon3);
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-;
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+/* Add the 3 Pokemon Objects to the IIFE pokemonList */
+
+pokemonRepository.add(pokemon1);
+pokemonRepository.add(pokemon2);
+pokemonRepository.add(pokemon3);
+
 
 /* Loop through the created Pokemons and show their names and heights
 add <p> elements to have better display with line breaks ;
 check also if height bigger 5.6 and show comment */
 
+
+/* Beforehand: Create a function to show Height Comment if height exceeds 5.6 feet height */
 
 function addComment(height) {
   if(!isNaN(parseFloat(height)) && isFinite(parseFloat(height))){
@@ -47,10 +60,16 @@ function addComment(height) {
   if(x>5.6){
   return ' - Wow, that\'s huge!'
   } else {return ''};
-}
+};
+
+/* Define Looping through heights function with help of forEach() */
 
 function myLoopFunction(pokemon) {
   document.write('<p>' + pokemon.pokemonName + ' (height: ' + pokemon.height + ')' + addComment(pokemon.height) + '</p>');
-}
+};
 
-pokemonList.forEach(myLoopFunction);
+
+
+/* Actually do the document.write() using IIFE and myLoopFunction() */
+
+pokemonRepository.getAll().forEach(myLoopFunction);
